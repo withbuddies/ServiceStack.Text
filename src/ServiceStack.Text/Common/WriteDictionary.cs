527f4db5b4fa2cce067b2fdf403de086bfd4d994
Brandon Cuff <brandon@scopely.com>
2015-02-25 21:03:52 -0800

    Fixed a bug where the serializer could get corrupted.
    This was happening in preview all over the place.
    This fix solved the problem (except for all the corrupted
    data that got left behind).

--- src/ServiceStack.Text/Common/WriteDictionary.cs
+++ src/ServiceStack.Text/Common/WriteDictionary.cs
@@ -124,21 +124,26 @@ namespace ServiceStack.Text.Common
 				JsWriter.WriteItemSeperatorIfRanOnce(writer, ref ranOnce);
 
 				JsState.WritingKeyCount++;
-				JsState.IsWritingValue = false;
+                try
+                {
+                    JsState.IsWritingValue = false;
 
-				if (encodeMapKey)
-				{
-					JsState.IsWritingValue = true; //prevent ""null""
-					writer.Write(JsWriter.QuoteChar);
-					writeKeyFn(writer, key);
-					writer.Write(JsWriter.QuoteChar);
-				}
-				else
-				{
-					writeKeyFn(writer, key);
-				}
-
-				JsState.WritingKeyCount--;
+                    if (encodeMapKey)
+                    {
+                        JsState.IsWritingValue = true; //prevent ""null""
+                        writer.Write(JsWriter.QuoteChar);
+                        writeKeyFn(writer, key);
+                        writer.Write(JsWriter.QuoteChar);
+                    }
+                    else
+                    {
+                        writeKeyFn(writer, key);
+                    }
+                }
+                finally
+                {
+                    JsState.WritingKeyCount--;
+                }
 
 				writer.Write(JsWriter.MapKeySeperator);
 
@@ -149,8 +154,14 @@ namespace ServiceStack.Text.Common
                 else
                 {
                     JsState.IsWritingValue = true;
-                    writeValueFn(writer, dictionaryValue);
-                    JsState.IsWritingValue = false;
+                    try
+                    {
+                        writeValueFn(writer, dictionaryValue);
+                    }
+                    finally
+                    {
+                        JsState.IsWritingValue = false;
+                    }
                 }
 			}
 
@@ -197,21 +208,26 @@ namespace ServiceStack.Text.Common
 				JsWriter.WriteItemSeperatorIfRanOnce(writer, ref ranOnce);
 
 				JsState.WritingKeyCount++;
-                JsState.IsWritingValue = false;
+                try
+                {
+                    JsState.IsWritingValue = false;
 
-				if (encodeMapKey)
-				{
-					JsState.IsWritingValue = true; //prevent ""null""
-					writer.Write(JsWriter.QuoteChar);
-					writeKeyFn(writer, kvp.Key);
-					writer.Write(JsWriter.QuoteChar);
-				}
-				else
-				{
-					writeKeyFn(writer, kvp.Key);
-				}
-				
-				JsState.WritingKeyCount--;
+                    if (encodeMapKey)
+                    {
+                        JsState.IsWritingValue = true; //prevent ""null""
+                        writer.Write(JsWriter.QuoteChar);
+                        writeKeyFn(writer, kvp.Key);
+                        writer.Write(JsWriter.QuoteChar);
+                    }
+                    else
+                    {
+                        writeKeyFn(writer, kvp.Key);
+                    }
+                }
+                finally
+                {
+                    JsState.WritingKeyCount--;
+                }
 
 				writer.Write(JsWriter.MapKeySeperator);
 
@@ -222,8 +238,14 @@ namespace ServiceStack.Text.Common
                 else
                 {
                     JsState.IsWritingValue = true;
-                    writeValueFn(writer, kvp.Value);
-                    JsState.IsWritingValue = false;
+                    try
+                    {
+                        writeValueFn(writer, kvp.Value);
+                    }
+                    finally
+                    {
+                        JsState.IsWritingValue = false;
+                    }
                 }
 			}
 
